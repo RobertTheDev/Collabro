5eef4c23a1be03472740e4de4b4c692e4cda810b
RobertTheDev <robertthedev@gmail.com>
2023-12-26 08:08:41 +0000

    feat: set up react konva

--- src/app/components/WhiteBoard/index.tsx
+++ src/app/components/WhiteBoard/index.tsx
@@ -1,9 +1,57 @@
-import styles from "./styles.module.css";
+"use client";
+import React, { useState } from "react";
+import { Stage, Layer, Line } from "react-konva";
 
-export default function WhiteBoard() {
+const Whiteboard = () => {
+  const [lines, setLines] = useState<any>([]);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const handleMouseDown = (e: any) => {
+    setIsDrawing(true);
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { points: [pos.x, pos.y] }]);
+  };
+
+  const handleMouseMove = (e: any) => {
+    if (!isDrawing) return;
+
+    const stage = e.target.getStage();
+    const pointerPos = stage.getPointerPosition();
+    const lastLine = lines[lines.length - 1];
+
+    if (lastLine) {
+      lastLine.points = lastLine.points.concat([pointerPos.x, pointerPos.y]);
+
+      setLines([...lines.slice(0, -1), lastLine]);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDrawing(false);
+  };
+
   return (
-    <div className={styles.screenContainer}>
-      <div className={styles.whiteboardContainer}></div>
-    </div>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMousemove={handleMouseMove}
+      onMouseup={handleMouseUp}
+    >
+      <Layer>
+        {lines.map((line: any, i: any) => (
+          <Line
+            key={i}
+            points={line.points}
+            stroke="black"
+            strokeWidth={5}
+            lineCap="round"
+            globalCompositeOperation="source-over"
+          />
+        ))}
+      </Layer>
+    </Stage>
   );
-}
+};
+
+export default Whiteboard;
