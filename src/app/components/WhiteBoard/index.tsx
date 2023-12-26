4fad6db9158a98f9eab2658c20a9e8bb54292019
RobertTheDev <robertthedev@gmail.com>
2023-12-26 04:46:21 +0000

    feat: created whiteboard component

---
+++ src/app/components/WhiteBoard/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export default function WhiteBoard() {
+  return (
+    <div className={styles.screenContainer}>
+      <div className={styles.whiteboardContainer}></div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import WhiteBoard from "./components/WhiteBoard";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <WhiteBoard />
     </div>
   );
 }
